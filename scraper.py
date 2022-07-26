7d2b741fbb671cc776571b15b8c2f0422028c8ca
dabravs <107572146+dabravs@users.noreply.github.com>
2022-07-26 12:12:17 -0400

    Initial commit

---
+++ scraper.py
@@ -0,0 +1,23 @@
+{
+ "cells": [
+  {
+   "cell_type": "markdown",
+   "id": "e3a31683",
+   "metadata": {},
+   "source": [
+    "# Open up a Jupyter notebook!!!!\n",
+    "\n",
+    "We're going to write a scraper!!!!"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 21,
+   "id": "f5d1d57d",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "import requests\n",
+    "from bs4 import BeautifulSoup\n",
+    "import pandas as pd"
+   ]
